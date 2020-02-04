dfcbc8d25cc4e9ed90257875fb5829eb6b79eb61
Yusuke Matsui <matsui528@gmail.com>
2020-02-04 16:29:45 +0900

    try to work with macos

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ class BuildExt(build_ext):
     }
 
     if sys.platform == 'darwin':
-        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7', '-fsized-deallocation']
 
     def build_extensions(self):
         ct = self.compiler.compiler_type
