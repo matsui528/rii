e1bcf6f821f694150c47d6d562ae6d9ccd284b10
Ash <11414900+ashleyabraham@users.noreply.github.com>
2022-03-02 17:54:06 -0500

    Update rii.h

--- src/rii.h
+++ src/rii.h
@@ -8,15 +8,15 @@
 
 // Handle missing ssize_t on Windows. Typedef's taken from:
 // https://msdn.microsoft.com/en-us/library/windows/desktop/aa383751%28v=vs.85%29.aspx#LONG_PTR
-#if defined(_WIN32) || defined(_WIN64)
-# if defined(_WIN64)
-typedef __int64 LONG_PTR;
-# else
-typedef long LONG_PTR;
+# if defined(_WIN32) || defined(_WIN64)
+    # if defined(_WIN64)
+        typedef __int64 LONG_PTR;
+    # else
+        typedef long LONG_PTR;
+    # endif
+    typedef LONG_PTR SSIZE_T;
+    typedef SSIZE_T ssize_t;
 # endif
-typedef LONG_PTR SSIZE_T;
-typedef SSIZE_T ssize_t;
-#endif
 
 // For py::array_t
 // See http://pybind11.readthedocs.io/en/master/advanced/pycpp/numpy.html#direct-access
